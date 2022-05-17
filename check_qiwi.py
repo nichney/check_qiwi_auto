63fe49eda3a839723e094358c95e6b95a27bf625
Kirill Osmolovsky <kirillosm09@gmail.com>
2022-05-17 11:08:47 +1000

    Заполнил документацию

--- check_qiwi.py
+++ check_qiwi.py
@@ -1,38 +1,59 @@
 """
 Модуль для выставления счетов и их автоматической проверки через QiwiAPI.
 Ключи доступа можно получить на p2p.qiwi.com
+
+Автор: Кирилл "Nichney" Осмоловский
+Год создания: 2021
+Сайт автора: https://nichney.ru
 """
 import sqlite3
 import requests
 from uuid import uuid4
+import logging
+import json
 
 
 class Check():
 
-    def __init__(self):
+    def __init__(self, hmKey = 0):
         """Для авторизации нужно указать publicKey и secretKey, созданные заранее на p2p.qiwi.com"""
+        logging.basicConfig(filename="log.txt", filemode="a", level=logging.DEBUG, format='%(asctime)s - %(message)s')
         f = open("config.txt")
         f = f.read().split("\n")
         self.publicKey = f[0]
         self.secret_key = f[1]
+        self.hmKey = hmKey
+        logging.debug("Public key && secret key succsessully loaded")
 
     def __generate_billid(self, user):
         result = str(uuid4())
+        logging.debug(f"Bill {result} was generated")
         con = sqlite3.connect('bill_ids.sqlite3')
         cur = con.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS Users (user INTEGER UNIQUE, bill TEXT)')
         cur.execute('INSERT OR IGNORE INTO Users values(?, ?)', (user, result))
+        logging.debug(f"Bill {result} was moved to table")
         con.commit()
         return result
 
-    def create_paylink(self, amount, user, comment=' '):
+    def __cutLink(self, link):
+        """Сокращает ссылку link"""
+        return requests.post("https://api.hm.ru/key/url/shorten", data=json.dumps({'api_key': self.hmKey, 'url': link})).json()['data']['short_url']
+
+    def create_paylink(self, amount: int, user: int, comment=' '):
         """Возвращает ссылку на оплату.
         Принимает стоимость счёта в рублях(amount),
                   комментарий для счёта(comment),
                   уникальный номер пользователя, который должен оплатить счёт. Генерируется на вашей стороне."""
-        return f'https://oplata.qiwi.com/create?publicKey={self.publicKey}&amount={amount}&billId={self.__generate_billid(user)}&comment={comment}'
+        logging.info(f"Payment for {user} for '{comment}' will creat")
+        return f'https://oplata.qiwi.com/create?publicKey={self.publicKey}&amount={amount}&billId={self.__generate_billid(user)}&comment="{comment}"'
 
-    def check_pay(self, user):
+    def create_short_paylink(self, amount: int, user: int, comment=' '):
+        """То же, что и create_paylink, но с сокращателем ссылки"""
+        link = self.create_paylink(amount, user, comment)
+        return self.__cutLink(link)
+
+    def check_pay(self, user: int) -> bool:
         """
         Проверка платежа по id пользователя, который вы указали при создании ссылки.
         Возвращает True, если счёт оплачен, и False, если не оплачен.
@@ -42,17 +63,24 @@ class Check():
         bill = cur.execute('SELECT bill FROM Users WHERE user = (?)', (user,)).fetchone()[0]
         r = requests.get(f'https://api.qiwi.com/partner/bill/v1/bills/{bill}',
                          headers={'accept': 'application/json', 'Authorization': f'Bearer {self.secret_key}'})
-        if r.json()['status']['value'] == 'PAID':
-            cur.execute(f'DELETE FROM Users WHERE user = {user}')
-            con.commit()
-            return True
-        else:
+        try:
+            if r.json()['status']['value'] == 'PAID':
+                cur.execute(f'DELETE FROM Users WHERE user = {user}')
+                logging.info(f"{user} payed for his payment")
+                con.commit()
+                return True
+            else:
+                logging.info(f"{user} didn't pay for his payment")
+        except KeyError:
+            logging.info(f"{user} didn't open his payment")
+        finally:
             return False
+        
 
 
 class Wallet():
 
-    def __init__(self, login, token):
+    def __init__(self, login: str, token: str):
         self.login = login
         self.token = token
 
